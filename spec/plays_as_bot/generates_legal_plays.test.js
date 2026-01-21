398c75da68f87b4ae2638aecf6bebbfd26790d02
Nikolas Martens <nikolas.m@rtens.org>
2026-01-21 20:27:00 +0100

    wip

--- spec/plays_as_bot/generates_legal_plays.test.js
+++ spec/plays_as_bot/generates_legal_plays.test.js
@@ -1,3 +1,15 @@
 import test from 'ava'
 
-test.todo('...')
+test.todo('empty board')
+
+test.todo('occupied square')
+
+test.todo('single piece')
+
+test.todo('limited directions')
+
+test.todo('lonely stack')
+
+test.todo('walled off')
+
+test.todo('wall smash')
