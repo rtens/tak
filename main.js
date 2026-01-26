f21521cd671c476dbc3bef67c78e628cb11fbc71
Nikolas Martens <martens@jenke-institut.de>
2026-01-26 13:24:24 +0100

    Use comma as input delimiter

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ import process from 'process'
 import Runner from './src/runner.js'
 import Interface from './src/interface.js'
 
-const args = process.argv.slice(2).join(' ').split('!').map(a => a.trim()).filter(a => !!a)
+const args = process.argv.slice(2).join(' ').split(',').map(a => a.trim()).filter(a => !!a)
 
 new Runner(new Interface(args)).run()
